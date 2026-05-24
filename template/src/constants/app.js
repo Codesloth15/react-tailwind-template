ee60ec18d885cc74bd36cc9a9a7a44967f5edec5
najexe <najlapuz@gmail.com>
2026-05-24 21:45:18 +0800

    "features added theme color"

---
+++ template/src/constants/app.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+    primary: "#25343F",     // Dark slate
+    secondary: "#BFC9D1",   // Soft blue-gray
+    background: "#EAEFEF",  // Light background
+    success: "#FF9B51",     // Orange accent
+    error: "#EF4444"        // Red error color
+}
